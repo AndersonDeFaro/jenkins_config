4cc235f2f5d23e376601dc1a80892c14a4fab77a
andersondefaro <anderson_faro@hotmail.com>
2024-02-14 14:40:59 -0300

    Subindo 1 versão demo ansible no jenkins

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -1,3 +1,8 @@
 pipeline {
-    
+    agent any
+    stages {
+        stage ('Execute exemplo 1 - ansible') {
+            ansiblePlaybook credentialsId: 'ssh_vagrant', installation: 'ansible', inventory: 'configs/ansible/hosts.conf', playbook: 'configs/ansible/playbooks/apt-update-all.yml', sudo: true, sudoUser: 'vagrant', vaultTmpPath: ''
+        }
+    }
 }
