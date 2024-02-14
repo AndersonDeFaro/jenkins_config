2419f4bf49fea0156703e95730124efce4cdcd08
andersondefaro <anderson_faro@hotmail.com>
2024-02-14 14:49:00 -0300

    Ajuste no script demo5 ansible v3

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('Execute exemplo 1 - ansible') {
             steps {
-                ansiblePlaybook credentialsId: 'ssh_vagrant', installation: 'ansible', inventory: 'configs/ansible/hosts.conf', playbook: 'configs/ansible/playbooks/apt-update-all.yml', sudo: true, sudoUser: 'vagrant', vaultTmpPath: ''
+                ansiblePlaybook become: true, becomeUser: 'vagrant', credentialsId: 'ssh_vagrant', installation: 'ansible', inventory: 'configs/ansible/hosts.conf', playbook: 'configs/ansible/playbooks/apt-update-all.yml', sudoUser: null, vaultTmpPath: ''
             }
         }
     }
