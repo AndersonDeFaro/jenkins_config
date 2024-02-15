f4a74f9841960db509a0dbb4d7b27cff805e9e51
andersondefaro <anderson_faro@hotmail.com>
2024-02-15 13:00:41 -0300

    Mudança naa forma de uso do ansible v3

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Execute 1 ansible') {
             steps {
-                sh 'ansible-playbook -i configs/ansible/hosts.conf --private-key=$ANSIBLE_PRIVATE_KEY configs/ansible/playbooks/mariadb.yml'
+                sh 'ansible-playbook -i configs/ansible/hosts.conf --private-key=$ANSIBLE_PRIVATE_KEY configs/ansible/playbooks/apt-update-all.yml'
             }
         }
     }
