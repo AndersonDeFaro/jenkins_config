e76639262f158685160bed43278d2b2c5afae431
andersondefaro <anderson_faro@hotmail.com>
2024-02-15 12:08:19 -0300

    ajustando caminho dos scripts

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -6,8 +6,8 @@ pipeline {
     stages {
         stage('Execute 1 ansible') {
             steps {
-                sh 'ansible-galaxy collection install -r ../ansible/requirements.yml'
-                sh 'ansible-playbook -i ../ansible/hosts.conf --private-key=$ANSIBLE_PRIVATE_KEY ../ansible/playbooks/mariadb.yml'
+                sh 'ansible-galaxy collection install -r configs/ansible/requirements.yml'
+                sh 'ansible-playbook -i configs/ansible/hosts.conf --private-key=$ANSIBLE_PRIVATE_KEY configs/ansible/playbooks/mariadb.yml'
             }
         }
     }
