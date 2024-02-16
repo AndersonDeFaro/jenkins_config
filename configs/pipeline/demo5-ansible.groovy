6a48bcd0279b22867bc9ffb41b36f61c416a257c
andersondefaro <anderson_faro@hotmail.com>
2024-02-15 21:33:58 -0300

    subindo versao demo5

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -1,12 +1,16 @@
 pipeline {
     agent any
-    environment {
-        ANSIBLE_PRIVATE_KEY=credentials('id-rsa-vagrant') 
-    }
     stages {
-        stage('Execute 1 ansible') {
+        stage ('Execute exemplo 1 - ansible') {
             steps {
-                sh 'ansible-playbook -i configs/ansible/hosts.conf --private-key=$ANSIBLE_PRIVATE_KEY configs/ansible/playbooks/apt-update-all.yml'
+                ansiblePlaybook become: true, 
+                                becomeUser: 'vagrant', 
+                                credentialsId: 'ssh_vagrant', 
+                                installation: 'ansible', 
+                                inventory: 'configs/ansible/hosts.conf', 
+                                playbook: 'configs/ansible/playbooks/apt-update-all.yml', 
+                                sudoUser: null, 
+                                vaultTmpPath: ''
             }
         }
     }
