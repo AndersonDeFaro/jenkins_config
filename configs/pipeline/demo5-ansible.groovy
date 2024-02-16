ab605aa19b738c0dc1b5710afad7a9c91efc23e2
andersondefaro <anderson_faro@hotmail.com>
2024-02-16 12:03:10 -0300

    removendo passo desnecessário

--- configs/pipeline/demo5-ansible.groovy
+++ configs/pipeline/demo5-ansible.groovy
@@ -1,12 +1,6 @@
 pipeline {
     agent any 
     stages {
-        stage ('Check gitHub') {
-            steps {
-                echo 'Checkout git repo'
-                git branch: 'main', credentialsId: 'gitHub', url: 'https://github.com/AndersonDeFaro/jenkins_config.git'
-            }
-        }
         stage ('Execute execute teste ansible') {
             steps {
                 ansiblePlaybook become: true, 
