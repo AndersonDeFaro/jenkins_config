4ea839ef1dc51297745825fadfaaf5779991b10a
andersondefaro <anderson_faro@hotmail.com>
2024-02-18 12:19:42 -0300

    Parte 2 da demo 6 refernte ao treinamento que estou curso alura que estou fazendo

---
+++ configs/pipeline/demo6-php-apache-p1.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any 
+    stages {
+		stage ('GitClone - gitHub') {
+			steps {
+				git branch: 'main', 
+					credentialsId: 'gitHub', 
+					url: 'https://github.com/AndersonDeFaro/jenkins_config.git'
+			}
+		}
+        stage ('Execute teste ansible - php8 apache2') {
+            steps {
+                ansiblePlaybook become: true, 
+                                credentialsId: 'ssh_vagrant', 
+                                disableHostKeyChecking: true, 
+                                installation: 'ansible', 
+                                inventory: 'configs/ansible/hosts.conf', 
+                                playbook: 'configs/ansible/playbooks/php-apache-itens.yml'
+            }
+        }
+    }
+}
