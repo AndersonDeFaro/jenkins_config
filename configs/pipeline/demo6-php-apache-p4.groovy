e80edf6089ce969f5788b6805512571300e1516b
andersondefaro <anderson_faro@hotmail.com>
2024-02-23 14:11:34 -0300

    Subindo Pipeline WORDPRESS - Apache Conf V2

--- configs/pipeline/demo6-php-apache-p4.groovy
+++ configs/pipeline/demo6-php-apache-p4.groovy
@@ -8,12 +8,19 @@ pipeline {
 					url: 'https://github.com/AndersonDeFaro/jenkins_config.git'
 			}
 		}
+		stage ('Atualizar arquivo apache2 Conf') {
+			steps {
+				echo 'Permissão - 000-default.conf'
+				sh 'chmod 770 ${WORKSPACE}/configs/files/000-default.conf'
+			}
+		}
         stage ('Execute ansible - install WORDPRESS') {
             steps {
                 ansiblePlaybook become: true, 
                                 credentialsId: 'ssh_vagrant', 
                                 disableHostKeyChecking: true, 
                                 installation: 'ansible', 
+                                extras: '--extra-vars "path_default=${WORKSPACE}/configs/files/000-default.conf"',
                                 inventory: 'configs/ansible/hosts.conf', 
                                 playbook: 'configs/ansible/playbooks/install-wordpress.yml'
             }
